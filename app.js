5f4e680a02659b09b6a8fa32986315560e489104
Arvind Kumar Mahto <arvind.kumar8271043313@gmail.com>
2020-10-01 11:09:39 +0530

    events and event handling rolling the dice

--- app.js
+++ app.js
@@ -15,11 +15,17 @@ scores = [0,0];
 roundscore=0;
 activePlayer=1;
 
-dice=Math.floor(Math.random()*6)+1;
-
-document.querySelector('#current-'+ activePlayer).textContent=dice;
-
-var x=document.querySelector('#score-0').textContent;
-console.log(x);
-
 document.querySelector('.dice').style.display='none';
+
+document.getElementById('score-0').textContent='0';
+document.getElementById('score-1').textContent='0';
+document.getElementById('current-0').textContent='0';
+document.getElementById('current-1').textContent='0';
+
+document.querySelector('.btn-roll').addEventListener('click',function(){
+    var dice=Math.floor(Math.random()*6)+1;
+    var diceDom=document.querySelector('.dice');
+    diceDom.style.display='block';
+    diceDom.src='dice-'+dice+'.png';
+
+});
