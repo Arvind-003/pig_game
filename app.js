080ffccaa1e9d6960584bb1ffab8c35971c73fa6
Arvind Kumar Mahto <arvind.kumar8271043313@gmail.com>
2020-10-01 11:39:51 +0530

    update score and changing active player

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var scores,roundscore,activePlayer,dice;
 
 scores = [0,0];
 roundscore=0;
-activePlayer=1;
+activePlayer=0;
 
 document.querySelector('.dice').style.display='none';
 
@@ -27,5 +27,28 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
     var diceDom=document.querySelector('.dice');
     diceDom.style.display='block';
     diceDom.src='dice-'+dice+'.png';
+
+    if(dice>1){
+        //add score
+        roundscore+=dice;
+        document.querySelector('#current-'+activePlayer).textContent=roundscore;
+    }else{
+        //next player 
+        activePlayer===0?activePlayer=1:activePlayer=0;
+        roundscore=0;
+
+        document.getElementById('current-0').textContent='0';
+        document.getElementById('current-1').textContent='0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+       // document.querySelector('.player-0-panel').classList.remove('active');
+       // document.querySelector('.player-1-panel').classList.add('active');
+
+       document.querySelector('.dice').style.display='none';
+        
+
+    }
 
 });
