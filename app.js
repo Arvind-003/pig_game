0401f016fe701cc8fe0157a907762787258607ab
Arvind Kumar Mahto <arvind.kumar8271043313@gmail.com>
2020-10-02 10:15:36 +0530

    win score=50

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ GAME RULES:
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
+- The first player to reach 50 points on GLOBAL score wins the game
 
 */
 
@@ -48,7 +48,7 @@ document.querySelector('.btn-hold').addEventListener('click',function(){
     document.querySelector('#score-'+activePlayer).textContent=scores[activePlayer];
     //check if player won the game
 
-        if(scores[activePlayer]>=20){
+        if(scores[activePlayer]>=50){
             document.querySelector('#name-'+activePlayer).textContent='Winner!!';
 
             document.querySelector('.dice').style.display='none';
